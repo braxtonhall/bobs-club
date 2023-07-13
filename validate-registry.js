cec8a569ddb593b20ba9a5dfe338fc2645b8e2cd
Braxton Hall <35436247+braxtonhall@users.noreply.github.com>
2023-07-13 21:16:23 +0200

    Validate the registry on PR (#11)

---
+++ validate-registry.js
@@ -0,0 +1,17 @@
+const registry = require("./registry.json");
+
+const isValidEntry = ([id, link]) => {
+	try {
+		new URL(link);
+		return /[a-zA-Z0-9-_]+/.test(id);
+	} catch {
+		return false;
+	}
+};
+
+const invalidEntries = Object.entries(registry)
+	.filter((entry) => !isValidEntry(entry));
+
+if (invalidEntries.length) {
+	throw new Error(`Invalid entries detected: ${JSON.stringify(invalidEntries)}`);
+}
