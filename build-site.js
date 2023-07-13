0a56da4bc88339f34e04ddb925f986ab8efeb8be
braxtonhall <braxtonjhall@gmail.com>
2023-07-13 19:43:48 +0200

    A few changes

--- build-site.js
+++ build-site.js
@@ -1,30 +1,20 @@
 const fs = require("fs-extra");
-const path = require("path");
+const registry = require("./registry.json");
 
 const BUILD_DIR = "./build";
 const STATIC_DIR = "./static";
-const REGISTRY_FILENAME = "registry.json";
+const TEMPLATE_DIR = "./templates";
 
 /**
- * @returns {Promise<{[id: string]: string}>}
+ * @type  {Array<{id: string, link: string}>}
  */
-const getRegistry = () =>
-	fs.readJson(path.join(STATIC_DIR, REGISTRY_FILENAME));
-
-/**
- * @returns {Promise<Array<{id: string, link: string}>>}
- */
-const getRegistryList = () =>
-	getRegistry()
-		.then((registry) => Object.entries(registry))
-		.then((entries) => entries.map(([id, link]) => ({id, link})));
+const registryList = Object.entries(registry).map(([id, link]) => ({id, link}));
 
 /**
  *
- * @param registryList {Array<{id: string, link: string}>}
  * @return Array<{pred: {id: string, link: string}, current: {id: string, link: string}, succ: {id: string, link: string}}>
  */
-const attachNeighbours = (registryList) =>
+const attachNeighbours = () =>
 	registryList.map(
 		(entry, index) => ({
 			pred: registryList[(index - 1 + registryList.length) % registryList.length],
@@ -33,25 +23,36 @@ const attachNeighbours = (registryList) =>
 		})
 	);
 
-const getRedirectHtml = (dst) =>
-`<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<title>bob's club</title>
-		<meta http-equiv="refresh" content="0; url='${dst}'" />
-		<script type="text/javascript">
-			window.location.href = "${dst}";
-		</script>
-	</head>
-	<body>
-		<p>thank you for visiting <a href="/">bob's club</a>!</p>
-		<p>if you're not redirected automatically, <a href="${dst}">click here</a></p>
-	</body>
-</html>
-`
+const getStaticRedirectHtml = (dst) =>
+	renderTemplate('redirect-static.html', {DST: dst});
 
-const writeRedirect = (src, dst) =>
-	fs.outputFile(`${BUILD_DIR}/${src}`, getRedirectHtml(dst));
+const getRandomRedirectHtml = (entries) =>
+	renderTemplate('redirect-random.html', {ENTRIES: JSON.stringify(entries)});
+
+/**
+ *
+ * @param name {string}
+ * @param env {Record<string, string>}
+ */
+const renderTemplate = (name, env) =>
+	fs.readFile(`${TEMPLATE_DIR}/${name}`)
+		.then((buffer) => buffer.toString())
+		.then(
+			(content) =>
+				Object.entries(env).reduce(
+					(render, [key, value]) =>
+						render.replaceAll(`$${key}`, value),
+					content
+				)
+		);
+
+const writeStaticRedirect = (src, dst) =>
+	getStaticRedirectHtml(dst)
+		.then((render) => fs.outputFile(`${BUILD_DIR}/${src}`, render));
+
+const writeRandomRedirect = (src, entries) =>
+	getRandomRedirectHtml(entries)
+		.then((render) => fs.outputFile(`${BUILD_DIR}/${src}`, render));
 
 /**
  * @param ring {Array<{pred: {id: string, link: string}, current: {id: string, link: string}, succ: {id: string, link: string}}>}
@@ -59,9 +60,10 @@ const writeRedirect = (src, dst) =>
 const writeRing = (ring) =>
 	Promise.all(
 		ring.flatMap(({pred, current, succ}) => [
-				writeRedirect(`/${current.id}/pred/index.html`, `../../${pred.id}`),
-				writeRedirect(`/${current.id}/succ/index.html`, `../../${succ.id}`),
-				writeRedirect(`/${current.id}/index.html`, current.link),
+				writeStaticRedirect(`/site/${current.id}/pred/index.html`, `../../${pred.id}`),
+				writeRandomRedirect(`/site/${current.id}/random/index.html`, registryList.filter(({id}) => id !== current.id)),
+				writeStaticRedirect(`/site/${current.id}/succ/index.html`, `../../${succ.id}`),
+				writeStaticRedirect(`/site/${current.id}/index.html`, current.link),
 			]
 		)
 	);
@@ -75,7 +77,7 @@ const copyStaticFiles = () =>
 const main = () =>
 	cleanBuild()
 		.then(copyStaticFiles)
-		.then(getRegistryList)
+		.then(() => writeRandomRedirect("/random/index.html", registryList))
 		.then(attachNeighbours)
 		.then(writeRing);
 
